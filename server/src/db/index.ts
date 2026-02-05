f85f2cc714af53f1fe374106f6c8b81af3253887
PedroRA-dev <123169057+PedroRA-dev@users.noreply.github.com>
2026-02-05 14:00:01 +0100

    Delete server directory

--- server/src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import Database from 'better-sqlite3';
-import path from 'path';
-
-const dbPath = path.join(__dirname, '../../database.db');
-export const db: Database.Database = new Database(dbPath);
-
-// Enable WAL mode for better performance
-db.pragma('journal_mode = WAL');
